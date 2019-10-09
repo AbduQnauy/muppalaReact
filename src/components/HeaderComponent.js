f4d4a7644b5eba1d6409f94eb2fb069c5c201a4c
Abdu Qnauy <abduqnauy@gmail.com>
2019-10-09 08:36:16 +0200

    Header and Footer3

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,14 +7,14 @@ class Header extends Component {
       <React.Fragment>
         <Navbar dark>
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Restaurant confusion</NavbarBrand>
           </div>
         </Navbar>
         <Jumbotron>
           <div className="container">
             <div className="row row-header">
               <div className="col-12 col-sm-6">
-                <h1>Ristorante con Fusion</h1>
+                <h1>Restaurant confusion</h1>
                 <p>
                   We take inspiration from the World's best cuisines, and create
                   a unique fusion experience. Our lipsmacking creations will
--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,32 +1,3 @@
-// import React, { Component } from "react";
-// import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-
-// class Menu extends Component {
-//   render() {
-//     const menu = this.props.dishes.map(dish => (
-//       <div key={dish.id} className="col-12 col-md-5 m-1">
-//         <Card
-//           style={{ cursor: "pointer" }}
-//           onClick={() => this.props.onClick(dish.id)}
-//           tag="li"
-//         >
-//           <CardImg width="100%" object src={dish.image} alt={dish.name} />
-//           <CardImgOverlay>
-//             <CardTitle>{dish.name}</CardTitle>
-//           </CardImgOverlay>
-//         </Card>
-//       </div>
-//     ));
-//     return (
-//       <div className="container">
-//         <div className="row">{menu}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Menu;
-
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
